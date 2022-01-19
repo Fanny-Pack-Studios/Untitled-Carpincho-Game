6decdddd7f9efd12106e02cdd1ae24fb6a3b296c
pablitar <pablitar@gmail.com>
2022-01-19 15:11:39 -0300

    Importado nuevo modelo de capybara + refactorizado script capybara

--- godot-project/assets/character/Capybara.gd
+++ godot-project/assets/character/Capybara.gd
@@ -7,20 +7,16 @@ export var speed: int = 2
 
 onready var camera = $"../CameraPivot/Camera"
 
+var input_direction_3d: Vector3 = Vector3()
+
 func _physics_process(delta):
-	var camera_forward = get_camera_forward()
+	
 	velocity.y -= gravity * delta
 	
 	if is_on_floor() && Input.is_action_just_pressed("jump"):
 		velocity.y = 4
 	
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	
-	if input_direction != Vector2.ZERO:
-		var input_direction_3d = Vector3(input_direction.x, 0, input_direction.y).normalized()
-		
-		input_direction_3d = input_direction_3d.rotated(Vector3.UP, camera_forward.signed_angle_to(Vector3.FORWARD, Vector3.DOWN))
-		
+	if input_direction_3d != Vector3.ZERO:
 		transform.basis = Basis(input_direction_3d.rotated(Vector3.UP, -PI/2), Vector3.UP, -input_direction_3d)
 		
 		var forward_velocity = speed * -global_transform.basis.z
@@ -31,6 +27,18 @@ func _physics_process(delta):
 		velocity.z = 0
 		
 	velocity = move_and_slide(velocity, Vector3.UP)
+
+func _process(delta):
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	if input_direction != Vector2.ZERO:
+		var camera_forward = get_camera_forward()
+		input_direction_3d = Vector3(input_direction.x, 0, input_direction.y).normalized()
+		
+		input_direction_3d = input_direction_3d.rotated(Vector3.UP, camera_forward.signed_angle_to(Vector3.FORWARD, Vector3.DOWN))
+	else:
+		input_direction_3d = Vector3.ZERO
+		
 
 func get_camera_forward() -> Vector3: 
 	var zbasis = camera.global_transform.basis.z
